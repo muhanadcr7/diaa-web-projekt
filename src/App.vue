402dd506db22af1dab491d705a6b3aaa88e19423
Muhanad Khatib <Muhanad.Khatib@student.hs-rm.de>
2025-10-19 20:38:08 +0200

    Erster Commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p>
+  Die Webseite ist in der Bearbeitung
+  </p>
+</template>
+
+<style scoped>
+p {
+  display: flex;
+  justify-content: center;  
+  align-items: center;     
+  height: 100vh;           
+  text-align: center;
+
+}
+</style>
